b3ad5a81a5f23c4374cf8b18933c99e860a481b1
samuele <s.campanella3@campus.unimib.it>
2024-01-13 22:05:42 +0100

    adsr envelope

--- main.cpp
+++ main.cpp
@@ -1,9 +1,93 @@
 #include <iostream>
+#include <atomic>
 #include "olcNoiseMaker.h"
 
-double frequency = 0.0;
+struct envelopeADSR {
+
+    double attack_time;
+    double decay_time;
+    double release_time;
+
+    double start_amplitude;
+    double sustain_amplitude;
+
+    double trigger_on_time;
+    double trigger_off_time;
+
+    bool note_is_on;
+
+    envelopeADSR() {
+        
+        attack_time = 10;
+        decay_time = 1;
+        release_time = 0.02;
+        start_amplitude = 1.0;
+        sustain_amplitude = 0.3;
+        trigger_on_time = 0.0;
+        trigger_off_time = 0.0;
+        note_is_on = false;
+
+    }
+
+    double get_amplitude(double time) {
+
+        double amplitude = 0.0;
+        double lifetime = time - trigger_on_time;
+
+        if (note_is_on) {
+            
+            // attack
+            if (lifetime <= attack_time) {
+                amplitude = ( lifetime / attack_time) * start_amplitude;
+            }
+
+            // decay
+            if (lifetime > attack_time && lifetime <= (attack_time + decay_time)) {
+                amplitude = ((lifetime - attack_time) / decay_time) * 
+                    (sustain_amplitude - start_amplitude) + start_amplitude;
+            }
+
+            // sustain
+            if (lifetime > (attack_time + decay_time)) {
+                amplitude = sustain_amplitude;
+            }
+
+        } else {
+
+            // release
+            amplitude = ((time - trigger_off_time) / release_time) *
+                (0.0 - sustain_amplitude) + sustain_amplitude;
+
+        }
+
+        if (amplitude <= 0.0001) {
+            amplitude = 0.0;
+        }
+
+        return amplitude;
+
+    }
+
+    void note_on(double time_on) {
+
+        trigger_on_time = time_on;
+        note_is_on = true;
+
+    }
+
+    void note_off(double time_off) {
+
+        trigger_off_time = time_off;
+        note_is_on = false;
+
+    }
+
+};
+
+std::atomic<double> frequency(0.0);
 double octave_base_freq = 110; // A2
 double d12th_root_of_2 = pow(2.0, 1.0 / 12.0);
+envelopeADSR envelope;
 
 void print_keyboard() {
 
@@ -39,7 +123,7 @@ double osc(double hertz, double time, int type) {
             {
                 double output = 0.0;
                 
-                for (double i = 1.0; i < 100.0; ++i) {
+                for (double i = 1.0; i < 70.0; ++i) {
                     output += ( sin(i * w(hertz) * time)) / i;
                 }
 
@@ -63,9 +147,9 @@ double osc(double hertz, double time, int type) {
 // ritorna ampiezza (tra -1 e 1) in funzione del tempo
 double make_noise(double time) {
 
-    double output = osc(frequency, time, 3);
+    double output = envelope.get_amplitude(time) * osc(frequency, time, 1);
 
-    return output * 0.5; // lower volume
+    return output; // lower volume
 
 }
 
@@ -73,6 +157,7 @@ int main() {
 
     // get all sound hw
     vector<wstring> devices = olcNoiseMaker<short>::Enumerate();
+
 
     // display
     for (auto d: devices) wcout << "Found Output Device: " << d << endl;
@@ -85,19 +170,22 @@ int main() {
 
     print_keyboard();
 
-    while (1) {
+    while (true) {
 
         bool key_pressed = false;
+
         for (int i = 0; i < 13; ++i) {
             if ( GetAsyncKeyState( (unsigned char)("ZSXCFVGBNJMK\xbc")[i] ) && 0x8000 ) {
                 
                 frequency = octave_base_freq * pow(d12th_root_of_2, i);
+                envelope.note_on(sound.GetTime());
                 key_pressed = true;
+
             }
         }
 
         if (!key_pressed) {
-            frequency = 0.0;
+            envelope.note_off(sound.GetTime());
         }
 
     }
